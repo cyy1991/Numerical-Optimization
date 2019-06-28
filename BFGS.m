30e3155f6644dc293e628e89e3718c23f8dea636
Ziyi <shangyuyu@sjtu.edu.cn>
2019-06-28 10:43:06 +0800

    temp
    last night's unfinished work

--- BFGS.m
+++ BFGS.m
@@ -1,7 +1,6 @@
 function [minf, l_1] = BFGS(m, lam0, preci, maxIt)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% f: target function
-% grad: gradient of f
+%
 % lam0: initial value of lambda 
 % preci: precision 
 % maxIt: maximum iteration
@@ -13,8 +12,20 @@ function [minf, l_1] = BFGS(m, lam0, preci, maxIt)
     if size(lam0, 1) == 1
         lam0 = transpose(lam0);
     end
+    
+    % the integrand part in 'f'
+    f_int = @(x, lam, n) exp(lam'*power(x, 0:n-1)');
+    % build the core function
+    f = @(lam, m, n) integral_impl(@(x) f_int(x, lam, n), 0, 1) - lam'*m;
+    % the jacobian function (hessian)
+    g_int = @(i, j, x, lam, n) x.^(i+j).*exp(lam'*power(x, 0:n-1)');
+    g = @(i, j, lam, n) integral_impl(@(x) g_int(i, j, x, lam, n), 0, 1);
+    % partial derivative function (gradient)
+    p_int = @(i, x, lam, n) x.^i.*exp(lam'*power(x, 0:n-1)');
+    p = @(i, lam, m, n) integral_impl(@(x) p_int(i, x, lam, n), 0, 1) - m(i+1);
+
     % initial two steps created by simple gradient descent
-    n=length(lam0);
+    n = length(lam0);
     y_2 = zeros(n, 1); % y_k-2
     y_1 = zeros(n, 1); % y_k-1
     for i = 0:n-1
@@ -43,8 +54,9 @@ function [minf, l_1] = BFGS(m, lam0, preci, maxIt)
     % last step hessian  (could be replaced?)
     D_last = eye(n);  % D denotes the inverse of hessian
 
-    %% Quasi Newton
-    iter = 0;
+    %% Quasi Newton with BFGS implementation
+    
+    it = 0;
     delta_y = y_1 - y_2;
     while abs(f_cur - f_last) > preci  % note precision is used when comparing gradient change to save computation cost of f(l), may not be optimal 
         
@@ -60,8 +72,8 @@ function [minf, l_1] = BFGS(m, lam0, preci, maxIt)
             
             y_1(i+1) = grad(i, l_1, m, n) ;
         end
-        iter = iter + 1;
-        if iter > maxIt
+        it = it + 1;
+        if it > maxIt
             
             warning('reach maximum iteration, process terminated');
             break 
@@ -70,6 +82,6 @@ function [minf, l_1] = BFGS(m, lam0, preci, maxIt)
         f_cur = f(l_1, m, n);
     end
 
-    display(iter)
+    display(it)
     minf = f(l_1, m, n);
 end
--- BFGS_test.m
+++ BFGS_test.m
@@ -1,16 +1,6 @@
 % m = load('data/m1.txt');
 m = [29.7352; 0.1439; 0.1257; 0.1117; 0.1004];
 
-% the integrand part in 'f'
-f_int = @(x, lam, n) exp(lam'*power(x, 0:n-1)');
-% build the core function
-f = @(lam, m, n) integral_impl(@(x) f_int(x, lam, n), 0, 1) - lam'*m;
-% the jacobian function
-g_int = @(i, j, x, lam, n) x.^(i+j).*exp(lam'*power(x, 0:n-1)');
-g = @(i, j, lam, n) integral_impl(@(x) g_int(i, j, x, lam, n), 0, 1);
-% partial derivative function
-p_int = @(i, x, lam, n) x.^i.*exp(lam'*power(x, 0:n-1)');
-p = @(i, lam, m, n) integral_impl(@(x) p_int(i, x, lam, n), 0, 1) - m(i+1);
 
 
 % l0 = -ones(101,1);
